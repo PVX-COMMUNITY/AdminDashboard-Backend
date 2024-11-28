c5e26170d12f8db88d2d709844e0cccd2c92bb50
dhruvpvx <dheerajgourdlmg1@gmail.com>
2024-11-29 00:26:59 +0530

    add birthday create validation

--- app/controllers/birthdays_controller.ts
+++ app/controllers/birthdays_controller.ts
@@ -1,5 +1,6 @@
 import { HttpContext } from '@adonisjs/core/http'
 import Birthday from '#models/birthday'
+import Member from '#models/member'
 
 export default class BirthdaysController {
   async index({ response }: HttpContext) {
@@ -9,6 +10,16 @@ export default class BirthdaysController {
 
   async store({ request, response }: HttpContext) {
     const data = request.only(['memberjid', 'name', 'username', 'date', 'month', 'year', 'place'])
+
+    const bday = await Birthday.query().where('memberjid', data.memberjid).first()
+    if (bday) {
+      return response.status(400).json({ message: 'Birthday already exists for this member' })
+    }
+
+    const member = await Member.find(data.memberjid)
+    if (!member) {
+      return response.status(400).json({ message: 'Member not found' })
+    }
 
     const birthday = await Birthday.create(data)
     await birthday.refresh()
